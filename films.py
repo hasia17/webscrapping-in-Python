90644d5f7fb2b8a93ca420a8256f81f3a4fae6c3
hasia17 <aaggaattaa.wan@gmail.com>
2020-08-26 20:38:03 +0200

    webscrapping

---
+++ films.py
@@ -0,0 +1,38 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def fetchUrl(url):
+  opener = request.build_opener()
+  opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0')]
+  request.install_opener(opener)
+  htmlSring = request.urlopen(url).read()
+  return htmlSring.decode()
+
+
+filmsHtml = fetchUrl('https://www.filmweb.pl/ranking/wantToSee/next30daysPoland')
+filmsSoup = BeautifulSoup(filmsHtml, features="html.parser")
+AllFilmsInfo = filmsSoup.find_all('div', {'class': 'item place'})
+
+filmsData = []
+
+
+for filmInfo in AllFilmsInfo:
+    filmData = []
+    filmTitle = filmInfo.find('a', {'class': 'film__link'}).text
+    premiereDate = filmInfo.find('div', {'class': 'film__premiere'}).text
+    wtsCounter = filmInfo.find('span', {'class': 'film__wts-count'}).text
+    productionYear = filmInfo.find('span', {'class': 'film__production-year'}).text[1:5]
+    filmData.append(filmTitle)
+    filmData.append(premiereDate)
+    filmData.append(wtsCounter)
+    filmData.append(productionYear)
+    filmsData.append(filmData)
+
+
+labels=['Title', 'Premiere Date', 'Year', 'People who want to see']
+df1=pd.DataFrame.from_records(filmsData, columns=labels)
+
+print(df1)
+
---
+++ movie.py
@@ -0,0 +1,24 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+
+def fetchUrl(url):
+  opener = request.build_opener()
+  opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0')]
+  request.install_opener(opener)
+  htmlSring = request.urlopen(url).read()
+  return htmlSring.decode()
+
+
+moviesHtml = fetchUrl('https://www.filmweb.pl/serials/premiere')
+moviesSoup = BeautifulSoup(moviesHtml, features="html.parser")
+AllMovieInfo = moviesSoup.find_all('div', {'class': 'premieresList__dayRange'})
+
+for movieInfo in AllMovieInfo:
+    movieTitle = movieInfo.find('h2', {'class': 'filmPreview__title'}).text
+    print(movieTitle)
+    premiereDate = movieInfo.find('h2', {'class': 'premieresList__dayHeader'})
+    premiereDate2 = premiereDate.find('time', {'class': 'formatDate'}).text
+    print(premiereDate)
+    print(premiereDate2)
+
